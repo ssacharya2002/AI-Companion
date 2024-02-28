a90bb90ac94dedc35858050e76ce61c157bf7ab0
Shaktishankar Acharya <86561217+ssacharya2002@users.noreply.github.com>
2024-02-28 12:25:44 +0530

    responsive sidebar added

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -8,6 +8,7 @@ import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "@/components/mode-toggle";
+import MobileSidebar from "./mobile-sidebar";
 
 const font = Poppins({
   weight: "600",
@@ -16,9 +17,11 @@ const font = Poppins({
 
 const Navbar = () => {
   return (
-    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary">
+    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16">
       <div className="flex items-center">
-        <Menu className="block md:hidden" />
+        <div className="block md:hidden">
+          <MobileSidebar />
+        </div>
         <Link href="/">
           <h1
             className={cn(
