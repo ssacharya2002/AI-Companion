a59578cfc0da21c70dd8eb258e44f106c686688a
Shaktishankar Acharya <86561217+ssacharya2002@users.noreply.github.com>
2024-03-04 02:22:04 +0530

    list of companion in search page

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,9 +1,39 @@
 import Categories from "@/components/categories";
+import Companions from "@/components/companions";
 import SearchInput from "@/components/search-input";
 import prismadb from "@/lib/prismadb";
-import { UserButton } from "@clerk/nextjs";
 
-const RootPage  = async() => {
+interface RootPageProps {
+    searchParams :{
+        categoryId:string;
+        name:string;
+    }
+}
+
+const RootPage  = async({
+    searchParams
+}:RootPageProps) => {
+
+
+    const data = await prismadb.companion.findMany({
+        where:{
+            categoryId:searchParams.categoryId,
+            name:{
+                search:searchParams.name
+            }
+        },
+        orderBy:{
+            createdAt:"desc"
+        },
+       include:{
+        _count :{
+            select:{
+                messages :true
+            }
+        } 
+       }
+    })
+
 
     const categories = await prismadb.category.findMany();
 
@@ -12,6 +42,7 @@ const RootPage  = async() => {
         <div className="w-full p-4 space-y-2">
             <SearchInput />
             <Categories data={categories} />
+            <Companions data={data} />
         </div>
      );
 }
