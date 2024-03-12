0ca0e10a6d85f0acb826c73d44bb6712f9edf4fe
Shaktishankar Acharya <86561217+ssacharya2002@users.noreply.github.com>
2024-03-12 21:35:37 +0530

    stripe payment api

---
+++ lib/subscription.ts
@@ -0,0 +1,39 @@
+import { auth } from "@clerk/nextjs";
+
+
+import prismadb from "@/lib/prismadb";
+
+const DAY_IN_MS = 86_400_000;
+
+export const checkSubscription = async () => {
+    const { userId } = auth();
+
+
+    if (!userId) {
+        return false;
+    }
+
+    const userSubscription = await prismadb.userSubscription.findUnique({
+        where: {
+            userId: userId,
+        },
+        select: {
+            stripeCurrentPeriodEnd: true,
+            stripePriceId: true,
+            stripeCustomerId: true,
+            stripeSubscriptionId: true
+        }
+    })
+
+
+    if (!userSubscription) {
+        return false;
+    }
+
+    const isValid = userSubscription.stripePriceId && userSubscription.stripeCurrentPeriodEnd?.getTime()! + DAY_IN_MS > Date.now()
+
+    // this ensure isValid is always a boolean
+    return !!isValid;
+
+
+}
